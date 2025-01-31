f79763842018b61b241282248bfd269e1e20cf21
renatocouto <renatocouto1867@gmail.com>
2025-01-31 16:09:52 -0300

    falta revisar o texto

--- app/src/main/java/com/example/renatocouto_atividade_08/ui/resumoprimeirovideo/PrimeiroVideoFragment.java
+++ app/src/main/java/com/example/renatocouto_atividade_08/ui/resumoprimeirovideo/PrimeiroVideoFragment.java
@@ -1,37 +1,79 @@
 package com.example.renatocouto_atividade_08.ui.resumoprimeirovideo;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.renatocouto_atividade_08.R;
 import com.example.renatocouto_atividade_08.databinding.FragmentPrimeiroVideoBinding;
+
+import java.util.Locale;
 
 public class PrimeiroVideoFragment extends Fragment {
 
     private FragmentPrimeiroVideoBinding binding;
+    private TextToSpeech textToSpeech;
+    private String msg;
 
+    @Nullable
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        PrimeiroVideoViewModel galleryViewModel =
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        PrimeiroVideoViewModel primeiroVideoViewModel =
                 new ViewModelProvider(this).get(PrimeiroVideoViewModel.class);
 
         binding = FragmentPrimeiroVideoBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        msg = getResources().getString(R.string.resumo_video_1);
 
-        final TextView textView = binding.textGallery;
-        galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        inicializarTextToSpeech();
+
+        binding.buttonLer.setOnClickListener(view -> lerResumo());
+
+        return binding.getRoot();
+    }
+
+    private void inicializarTextToSpeech() {
+        textToSpeech = new TextToSpeech(requireContext(), status -> {
+            if (status == TextToSpeech.SUCCESS) {
+                Locale locale = new Locale("pt", "BR");
+                int result = textToSpeech.setLanguage(locale);
+                textToSpeech.setSpeechRate(1.2f);
+                if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                    Log.e("TextToSpeech", "Idioma não suportado");
+                }
+            } else {
+                Log.e("TextToSpeech", "Falha na inicialização");
+            }
+        });
+    }
+
+    private void lerResumo() {
+        if (textToSpeech != null && !textToSpeech.isSpeaking()) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                textToSpeech.speak(msg, TextToSpeech.QUEUE_FLUSH, null, null);
+            } else {
+                textToSpeech.speak(msg, TextToSpeech.QUEUE_FLUSH, null);
+            }
+        }
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+        if (textToSpeech != null) {
+            textToSpeech.stop();
+            textToSpeech.shutdown();
+        }
         binding = null;
     }
 }
--- app/src/main/java/com/example/renatocouto_atividade_08/ui/resumoterceirovideo/TerceiroVideoFragment.java
+++ app/src/main/java/com/example/renatocouto_atividade_08/ui/resumoterceirovideo/TerceiroVideoFragment.java
@@ -1,15 +1,18 @@
 package com.example.renatocouto_atividade_08.ui.resumoterceirovideo;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.renatocouto_atividade_08.R;
 import com.example.renatocouto_atividade_08.databinding.FragmentTerceiroVideoBinding;
 
 public class TerceiroVideoFragment extends Fragment {
@@ -24,9 +27,42 @@ public class TerceiroVideoFragment extends Fragment {
         binding = FragmentTerceiroVideoBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-//        final TextView textView = binding.textSlideshow;
-//        galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        binding.buttonEnviar.setOnClickListener(view -> clicar());
+
+
         return root;
+    }
+
+    public void clicar() {
+        Intent intent = null;
+        String text = getResources().getString(R.string.resumo_video_3);
+
+        if (verficaApp()) {
+            intent = new Intent();
+            intent.setAction(Intent.ACTION_SEND);
+            intent.putExtra(Intent.EXTRA_TEXT, text);
+            intent.setType("text/plain");
+            intent.setPackage("com.whatsapp");
+            startActivity(intent);
+        } else {
+            abrirWhatsAppWeb();
+        }
+
+
+    }
+
+    private void abrirWhatsAppWeb() {
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://web.whatsapp.com/"));
+        startActivity(intent);
+    }
+
+    private boolean verficaApp() {
+        try {
+            requireActivity().getPackageManager().getPackageInfo("com.whatsApp", PackageManager.GET_ACTIVITIES);
+            return true;
+        } catch (PackageManager.NameNotFoundException e) {
+            return false;
+        }
     }
 
     @Override
